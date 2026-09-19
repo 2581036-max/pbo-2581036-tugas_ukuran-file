8cbd79f4ff9509cc9b899ce49c867dccb55d9670
Rahma Hidayanti <2581036@unai.edu>
2026-09-19 20:28:51 +0700

    Menambahkan Konstanta SATU_KB dan kalkulasi konversi KB, MB, Dan GB.

--- src/UkuranFile.java
+++ src/UkuranFile.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class UkuranFile {
+    public static final double SATU_KB = 1024.0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -10,9 +11,17 @@ public class UkuranFile {
         System.out.print("Ukuran (byte) : ");
         long ukuranByte = scanner.nextLong();
 
+        double ukuranKB = ukuranByte / SATU_KB;
+        double ukuranMB = ukuranKB / SATU_KB;
+        double ukuranGB = ukuranMB / SATU_KB;
+
         System.out.println("\n===== UKURAN FILE =====");
         System.out.println(namaFile);
         System.out.println(ukuranByte + " byte");
+
+        System.out.println(ukuranKB + " KB");
+        System.out.println(ukuranMB + " MB");
+        System.out.println(ukuranGB + " GB");
 
         scanner.close();
     }
